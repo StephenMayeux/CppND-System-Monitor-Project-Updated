10138082a733d6cf03e4640e269631d3288e476d
Stephen Mayeux <stephenmayeux@gmail.com>
2020-01-19 22:31:20 -0600

    save work

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,24 @@
 #include <string>
+#include <sstream>
+#include <iomanip>
 
 #include "format.h"
 
+#define HOUR 3600
+#define MIN 60
+
 using std::string;
 
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) { 
+    int hh = seconds / HOUR;
+    seconds %= HOUR;
+    int mm = seconds / MIN;
+    seconds %= MIN;
+    int ss = seconds;
+
+    std::stringstream result;
+    result << std::setfill('0') << std::setw(2) << hh << ":"
+        << std::setfill('0') << std::setw(2) << mm << ":"
+        <<  std::setfill('0') << std::setw(2) << ss;
+    return result.str();
+}
--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -10,7 +10,6 @@ using std::string;
 using std::to_string;
 using std::vector;
 
-// DONE: An example of how to read data from the filesystem
 string LinuxParser::OperatingSystem() {
   string line;
   string key;
@@ -33,15 +32,14 @@ string LinuxParser::OperatingSystem() {
   return value;
 }
 
-// DONE: An example of how to read data from the filesystem
 string LinuxParser::Kernel() {
-  string os, kernel;
+  string os, version, kernel;
   string line;
   std::ifstream stream(kProcDirectory + kVersionFilename);
   if (stream.is_open()) {
     std::getline(stream, line);
     std::istringstream linestream(line);
-    linestream >> os >> kernel;
+    linestream >> os >> version >> kernel;
   }
   return kernel;
 }
@@ -70,7 +68,17 @@ vector<int> LinuxParser::Pids() {
 float LinuxParser::MemoryUtilization() { return 0.0; }
 
 // TODO: Read and return the system uptime
-long LinuxParser::UpTime() { return 0; }
+long LinuxParser::UpTime() {
+  long systemUptime, idleTime;
+  string line;
+  std::ifstream stream(kProcDirectory + kUptimeFilename);
+  if (stream.is_open()) {
+    std::getline(stream, line);
+    std::istringstream linestream(line);
+    linestream >> systemUptime >> idleTime;
+  }
+  return systemUptime + idleTime;
+}
 
 // TODO: Read and return the number of jiffies for the system
 long LinuxParser::Jiffies() { return 0; }
